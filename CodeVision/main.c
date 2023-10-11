39b16b8dcef6fe4e04d6bf4ba5df8714808732a0
tahoura morovati <t.morovati.99@gmail.com>
2023-10-11 18:20:51 +0330

    update

---
+++ CodeVision/main.c
@@ -0,0 +1,51 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 8/24/2020
+Author  : Fateme Akarami - 943186110
+Company : 
+Comments: 
+
+
+Chip type               : ATmega32
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 512
+*******************************************************/
+
+#include <mega32.h>
+#include <alcd.h>
+#include <delay.h>
+
+void main(void)
+{
+
+
+// Port C initialization
+// Function: Bit7=Out Bit6=Out Bit5=Out Bit4=Out Bit3=Out Bit2=Out Bit1=Out Bit0=Out 
+DDRC=(1<<DDC7) | (1<<DDC6) | (1<<DDC5) | (1<<DDC4) | (1<<DDC3) | (1<<DDC2) | (1<<DDC1) | (1<<DDC0);
+// State: Bit7=0 Bit6=0 Bit5=0 Bit4=0 Bit3=0 Bit2=0 Bit1=0 Bit0=0 
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+
+lcd_init(16);
+
+while (1)
+      {
+      lcd_clear();
+      lcd_gotoxy(0,0);
+      lcd_putsf("Fateme Akrami");
+      lcd_gotoxy(0,1);
+      lcd_putsf("943186110");
+      delay_ms(1000);
+
+      }
+}
